2e4369c352d6d4cc7a2ba8d3a20cb0ed00b5c996
Stephan Kasdorf <stephan.kasdorf@bittomine.com>
2020-03-30 00:21:59 +0200

    FIXED DESTINATION FOR HTTPS CERTS

--- gen.sh
+++ gen.sh
@@ -3,6 +3,8 @@ openssl genrsa 1024 > server.key
 chmod 400 server.key
 openssl req -new -x509 -nodes -sha1 -days 365 -key server.key -out server.crt
 
+mv server.* ./nginx/ca/
+
 #openssl genrsa -des3 -out server.key 1024
 #openssl req -new -key server.key -out server.csr
 #openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt
